9ba51809246c5586aed6ea4934f1066970ddf0de
dantekin79-creator <dantekin79@gmail.com>
2026-05-26 14:05:02 +0300

    Added missing useAuth import to SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../supabase';
+import { useAuth } from '../context/useAuth';
  
 import { useTheme } from '../context/useTheme';
 import { FiMail, FiLock, FiUser, FiArrowRight, FiLoader } from 'react-icons/fi';
